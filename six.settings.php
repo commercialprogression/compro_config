76895f6532b0e9d3a56dcf2d04ffeb93324bb530
Brad Czerniak <ao5357@gmail.com>
2015-11-03 15:17:38 -0500

    d6 settings file to support compro-settings

---
+++ six.settings.php
@@ -0,0 +1,21 @@
+<?php
+
+$db_url = 'mysql://root:PASSWORD@localhost/DBNAME';
+$db_prefix = '';
+
+ini_set('max_execution_time', 0);
+
+$update_free_access = FALSE;
+
+ini_set('arg_separator.output',     '&amp;');
+ini_set('magic_quotes_runtime',     0);
+ini_set('magic_quotes_sybase',      0);
+ini_set('session.cache_expire',     200000);
+ini_set('session.cache_limiter',    'none');
+ini_set('session.cookie_lifetime',  2000000);
+ini_set('session.gc_maxlifetime',   200000);
+ini_set('session.save_handler',     'user');
+ini_set('session.use_cookies',      1);
+ini_set('session.use_only_cookies', 1);
+ini_set('session.use_trans_sid',    0);
+ini_set('url_rewriter.tags',        '');
